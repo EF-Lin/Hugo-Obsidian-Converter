8aa0b28fba76aa0e85d4278cda9d1547f0a5df2b
EFLin <79702669+EF-Lin@users.noreply.github.com>
2025-02-09 20:11:31 +0800

    add argparse to __main__.py

---
+++ __main__.py
@@ -0,0 +1,15 @@
+import argparse
+from converter import Convert
+from reader import Glasses
+
+parser = argparse.ArgumentParser()
+parser.add_argument("read_path")
+parser.add_argument("write_path")
+# parser.add_argument("pic_path")
+args = parser.parse_args()
+
+g = Glasses(read_path=args.read_path, write_path=args.write_path, name_num=10)
+file = g.read_file()
+c = Convert(obsidian=file)
+data = c.convert()
+g.write_file(data)
