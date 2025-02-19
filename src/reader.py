8715c7ceecec487035f1ff045c4f40a4e660eaf4
EFLin <79702669+EF-Lin@users.noreply.github.com>
2025-02-20 00:42:47 +0800

    improve path determination

--- src/reader.py
+++ src/reader.py
@@ -19,21 +19,19 @@ class Glasses:
     default_write_path: str = ""
 
     def __post_init__(self):
-        self.read_path = self.path_condition(self.read_path, self.default_read_path)
+        self.read_path = self.add_filetype(self.path_condition(self.read_path, self.default_read_path))
         self.write_path = self.path_condition(self.write_path, self.default_write_path)
-        if self.name != "":
-            if self.name.find('.') == -1:
-                self.name += self.file_type
-        else:
+        if self.name == "":
             i = 0
             for n in range(len(self.read_path)):
                 if self.read_path[n] in ['/', '\\']:
                     i = n
             if self.name_num == -1:
-                self.name = self.read_path[i+1:]
+                self.name = self.read_path[i + 1:]
             else:
-                self.name = f"{self.read_path[i+1:i+self.name_num+1]}{self.file_type}"
+                self.name = f"{self.read_path[i + 1:i + self.name_num + 1]}{self.file_type}"
 
+        self.name = self.add_filetype(self.name)
         self.date = self.name[:self.name.find('.')] if self.sep_name else str(datetime.date.today())
         self.title = self.read_path[len(self.read_path) - self.read_path[::-1].find('-'):len(self.read_path) - 1 - self.read_path[::-1].find('.')] if self.sep_name else "New_file"
 
@@ -47,6 +45,10 @@ class Glasses:
             if default[0] == '/':
                 default = default[1:]
         return os.path.normpath(f"{default}{path}")
+
+    def add_filetype(self, path):
+        path += self.file_type if path.find('.') == -1 else ''
+        return path
 
     def read_file(self) -> list:
         return open(self.read_path, encoding="utf8").readlines()
@@ -68,3 +70,4 @@ class Glasses:
 if __name__ == "__main__":
     g = Glasses(read_path="./test/2024-03-30-uuu.md", write_path="./test/", name="ttt", sep_name=True)
     print(g.date)
+    print(g.add_filetype('C:/a'))
