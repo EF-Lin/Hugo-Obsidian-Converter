d2f545f40b91059fb6c20cd1aa17887df5839068
EFLin <79702669+EF-Lin@users.noreply.github.com>
2025-08-21 02:01:39 +0800

    small fix and add comments

--- src/converter.py
+++ src/converter.py
@@ -41,19 +41,24 @@ class Convert:
 
     @staticmethod
     def __scan(text: str) -> bool:
+        # 掃描有無需要注意的文內連結
         return True if re.search(r"\[\[.{1,}#", text) != None else False
 
     def __del_content(self):
+        # delete content
         self.obsidian = re.sub(r"```table-of-contents.*?```", "", self.obsidian, flags=re.DOTALL)
 
     def __pic(self):
+        # replace pic link
         self.obsidian = re.sub(r"!\[\[(.*?)\.(.*?)\]\]", r"![\1](" + self.pic_path + r'/\1.\2 "\1")', self.obsidian)
 
     def __link(self):
-        self.obsidian = re.sub(r"\[\[(.{1,}#.*?)\]\]",r"[\1](link)", self.obsidian)
+        # replace link, two formats
+        self.obsidian = re.sub(r"\[\[(.{1,})(#.*?)\]\]",r"[\1\2](link\2)", self.obsidian)
         self.obsidian = re.sub(r"\[\[#(.*?)\]\]", r"[\1](#\1)", self.obsidian)
 
     def __adm(self):
+        # replace admonition
         """
         adm_type = {
             "note": "note",
@@ -72,25 +77,29 @@ class Convert:
         self.obsidian = re.sub(r"> *?\[!(.*?)\](.*?)\n\n", lambda s: f'{{{{< admonition type="{"tip" if s.group(1) == "hint" else s.group(1)}" title="{s.group(1).capitalize()}" >}}}}{s.group(2).replace('>', '')}\n{{{{< /admonition >}}}}\n\n', self.obsidian, flags=re.DOTALL)
 
     def __sep_code(self):
+        # seperate code, \n -> \n\n
         self.obsidian = re.sub(r"```(.*?)\n", r"```\1{linenos=true}\n", self.obsidian)
         #self.obsidian = re.sub(r"```(.*?)\n", lambda s: r'```' if s.group(1)=='' else f"```{s.group(1)}{{linenos=true}}\n", self.obsidian)
         i = self.obsidian.find('\n')
         jump1 = -1
         jump2 = -1
         while i != -1 and i < len(self.obsidian)-1:
+            # jump if in ```
             jump1 = -jump1 if self.obsidian[i+1] == '`' else jump1
+            # jump if in {}, e.g. admonition
             jump2 = -jump2 if self.obsidian[i+1] == '{' else jump2
             if self.obsidian[i+1] not in self.allow_chars and self.obsidian[i-1] not in self.allow_chars and jump1 < 0 and jump2 < 0:
                 self.obsidian = self.obsidian[:i+1] + '\n' + self.obsidian[i+1:]
             elif jump1 > 0:
                 jump1 = -jump1
-            else:
+            elif jump2 > 0:
                 jump2 = -jump2
 
             i = self.obsidian.find('\n', i+1)
         self.obsidian = re.sub(r"\n```{linenos=true}", r"```", self.obsidian)
 
     def __highlight(self):
+        # replace highlight
         self.obsidian = re.sub(r"==(.*?)==", r"<mark>\1</mark>", self.obsidian)
 
 
