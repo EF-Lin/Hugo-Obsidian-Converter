5a25eae3fd188b01c5fc2c740b8d0ca89e99962a
EFLin <79702669+EF-Lin@users.noreply.github.com>
2025-08-21 15:17:12 +0800

    add lastdate

--- src/header.py
+++ src/header.py
@@ -1,5 +1,6 @@
 import os.path
 from dataclasses import dataclass
+import datetime
 from src.endl import list_endl
 from typing import ClassVar
 
@@ -11,12 +12,12 @@ class Head:
     main_data: str
     date: str
     title: str
+    temp: str = "default.md"
     default_path: ClassVar[str] = "/data/archetypes/"
-    temp: ClassVar[str] = "default.md"
 
     def __post_init__(self):
         self.path = os.path.normpath(f"{os.getcwd()}/Hugo-Obsidian-Converter/{self.default_path}/{self.temp}")
-        self.head_data = open(self.path, 'r').read()
+        self.head_data = open(self.path, 'r', encoding="utf-8").read()
 
     def add_head(self, is_date=True, is_title=True) -> str:
         self.__rep_date() if is_date else 0
@@ -24,10 +25,11 @@ class Head:
         return list_endl(self.head_data) + self.main_data
 
     def __rep_date(self):
-        self.head_data = self.head_data.replace("{{date}}", f"\"{self.date}\"")
+        self.head_data = self.head_data.replace("{{date}}", f"{self.date}")
+        self.head_data = self.head_data.replace("{{lastdate}}", str(datetime.date.today()))
 
     def __rep_title(self):
-        self.head_data = self.head_data.replace("{{title}}", f"\"{self.title}\"")
+        self.head_data = self.head_data.replace("{{title}}", f"{self.title}")
 
 
 if __name__ == "__main__":
