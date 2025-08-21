785c6fbe8493e97d73d8fb3b581d6194fce8a687
EFLin <79702669+EF-Lin@users.noreply.github.com>
2025-08-21 11:02:15 +0800

    add exception handing

--- __main__.py
+++ __main__.py
@@ -2,6 +2,7 @@ import argparse
 from src.converter import Convert
 from src.reader import Glasses
 from src.header import Head
+from rich import print
 
 parser = argparse.ArgumentParser()
 parser.add_argument("read_path")
@@ -9,10 +10,14 @@ parser.add_argument("write_path")
 # parser.add_argument("pic_path")
 args = parser.parse_args()
 
-g = Glasses(read_path=args.read_path, write_path=args.write_path, name_num=10, sep_name=True)
-file = g.read_file()
-c = Convert(obsidian=file)
-data = c.convert()
-h = Head(main_data=data, date=g.date, title=g.title)
-data = h.add_head()
-g.write_file(data)
+try:
+    g = Glasses(read_path=args.read_path, write_path=args.write_path, name_num=10, sep_name=True)
+    file = g.read_file()
+    c = Convert(obsidian=file)
+    data = c.convert()
+    h = Head(main_data=data, date=g.date, title=g.title)
+    data = h.add_head()
+    g.write_file(data)
+    print("[green]INFO: convert successful")
+except Exception as ex:
+    print(f"[red]ERROR: {ex}")
